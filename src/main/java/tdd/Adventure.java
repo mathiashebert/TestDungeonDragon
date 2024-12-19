39e379f1a2c205757bbc035dc3fa32c7218136be
mathias.hebert <mathias.hebert>
2024-12-19 14:19:15 +0100

    exemple - debut aventure

--- src/main/java/tdd/Adventure.java
+++ src/main/java/tdd/Adventure.java
@@ -10,6 +10,22 @@ public class Adventure {
      * @param inventaire l'inventaire du héro
      */
     public void aventure(Hero hero, Inventaire inventaire) {
+        hero.fouiller();
+
+        hero.avancer(); // squelette
+        hero.combattre(inventaire.epee().frapper());
+        hero.fouiller();
+
+        hero.avancer(); // zombies
+        hero.combattre(inventaire.lance().jeter(), inventaire.epee().frapper(), inventaire.bouclier().bloquer());
+        hero.fouiller();
+
+        inventaire.potion().boire(PotionMagique.Choix.ENDURANCE);
+        inventaire.rune().graver(inventaire.lance());
+        hero.avancer(); // areignée
+        hero.combattre(inventaire.manuelDeLAventurier().positionDefensive(), inventaire.lance().frapper(), inventaire.bouclier().bloquer());
+
+        hero.fouiller();
     }
 
 }
